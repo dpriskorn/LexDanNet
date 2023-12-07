353bc0f786854ea4ce22b53a472a76ce37e4a26e
Dennis Priskorn <egils@riseup.net>
2023-12-07 13:01:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LexDanNet
+Script to extract and add DanNet 2.2 word ID to Danish Wikidata lexemes
---
+++ main.py
@@ -0,0 +1,210 @@
+import logging
+import re
+
+import pandas as pd
+import requests
+import zipfile
+from io import BytesIO
+
+from pandas import DataFrame
+from pydantic import BaseModel, AnyHttpUrl
+from typing import List, Any
+from tqdm import tqdm
+from wikibaseintegrator import WikibaseIntegrator
+from wikibaseintegrator.datatypes import ExternalID
+from wikibaseintegrator.entities import LexemeEntity
+from wikibaseintegrator.wbi_helpers import execute_sparql_query
+from wikibaseintegrator.wbi_login import Login
+
+import config
+
+logging.basicConfig(level=config.loglevel)
+logger = logging.getLogger(__name__)
+
+class Form(BaseModel):
+    form: str
+    id: str
+
+
+class ZipFileHandler(BaseModel):
+    zip_file_path: AnyHttpUrl
+    zip_content: Any = BytesIO()
+    forms: List[Form] = []
+    unzipped_content: Any = BytesIO()
+    lexeme_ids: List[str] = list()
+    no_dannet_lexeme_ids: List[str] = list()
+    df: DataFrame = DataFrame()
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def download_zip(self) -> None:
+        response = requests.get(url=self.zip_file_path, stream=True)
+        if response.status_code != 200:
+            raise ValueError("Invalid URL or unable to download the file.")
+
+        total_size = int(response.headers.get('content-length', 0))
+
+        with tqdm(total=total_size, unit='B', unit_scale=True, desc='Downloading') as pbar:
+            for chunk in response.iter_content(chunk_size=1024):
+                self.zip_content.write(chunk)
+                pbar.update(len(chunk))
+
+    def unzip_content(self) -> None:
+        print("Unzipping content")
+        self.zip_content.seek(0)
+        self.unzipped_content = BytesIO()
+
+        with zipfile.ZipFile(self.zip_content, 'r') as zip_ref:
+            words_owl_files = [file_name for file_name in zip_ref.namelist() if file_name.endswith('words.rdf')]
+            if not words_owl_files:
+                raise ValueError("No 'words.owl' file found in the zip archive.")
+
+            words_owl_file = words_owl_files[0]
+            self.unzipped_content.write(zip_ref.read(words_owl_file))
+            self.unzipped_content.seek(0)
+
+    @property
+    def unzipped_data_exists(self) -> bool:
+        return bool(self.unzipped_content)
+
+    def print_head_unzipped_content(self) -> None:
+        print("Printing head of unzipped content")
+        self.check_unzipped_data()
+
+        lines_to_print = 30
+        for i, line in enumerate(self.unzipped_content.readlines()):
+            print(line.decode('ISO-8859-1').rstrip())
+            lines_to_print -= 1
+            if lines_to_print <= 0:
+                break
+
+    def check_unzipped_data(self):
+        if not self.unzipped_data_exists:
+            raise ValueError("No unzipped data")
+
+    def extract_forms(self) -> None:
+        """We use regex based extraction because we got weird errors when parsing the XML"""
+        print("Extracting forms")
+        self.check_unzipped_data()
+        forms_list = []
+
+        xml_content = self.unzipped_content.getvalue().decode('ISO-8859-1')
+
+        pattern = r'<wn20schema:Word rdf:about="&dn;word-(.*?)" wn20schema:lexicalForm="(.*?)"'
+        matches = re.findall(pattern, xml_content, re.DOTALL)
+
+        if not matches:
+            raise ValueError("No matches found")
+
+        for match in matches:
+            form_id, form = match
+            forms_list.append(Form(form=form, id=form_id))
+
+        if not forms_list:
+            raise ValueError("Found no forms")
+
+        self.forms = forms_list
+        print(f"{len(self.forms)} forms extracted")
+
+    def create_dataframe_and_export_csv(self):
+        print("Creating dataframe")
+        self.check_unzipped_data()
+        df = pd.DataFrame(self.forms, columns=['id', 'form'])
+
+        # Print info about the DataFrame
+        print(df.info())
+
+        # Export DataFrame to a CSV file
+        df.to_csv('forms.csv', index=False)
+        self.df = df
+
+    def fetch_danish_lexeme_ids(self):
+        # SPARQL query to retrieve Danish lexeme IDs
+        sparql_query = """
+        SELECT ?lexeme WHERE {
+            ?lexeme dct:language wd:Q9035.
+        }
+        """
+
+        # Run the SPARQL query
+        query_result = execute_sparql_query(sparql_query)
+
+        danish_lexeme_ids = [result['lexeme']['value'] for result in query_result['results']['bindings']]
+
+        self.lexeme_ids = danish_lexeme_ids
+        print(f"Fetched {len(self.lexeme_ids)} danish lexemes from Wikidata")
+
+    def fetch_danish_lexeme_ids_without_dannet_property(self):
+        # SPARQL query to retrieve Danish lexeme IDs without the "dannet" property
+        sparql_query = """
+        SELECT ?lexeme WHERE {
+            ?lexeme dct:language wd:Q9035.
+            FILTER NOT EXISTS {
+                ?lexeme wdt:P6140 ?dannetProperty.
+            }
+        }
+        """
+
+        # Run the SPARQL query
+        query_result = execute_sparql_query(sparql_query)
+
+        danish_lexeme_ids_without_dannet_property = [result['lexeme']['value'] for result in
+                                                     query_result['results']['bindings']]
+
+        self.no_dannet_lexeme_ids = danish_lexeme_ids_without_dannet_property
+        print(f"Fetched {len(self.no_dannet_lexeme_ids)} danish lexemes from Wikidata")
+
+    def find_dannet_ids_for_lexemes(self):
+        """Match forms and upload matches to Wikidata"""
+        if not hasattr(self, 'no_dannet_lexeme_ids'):
+            raise ValueError("No list of lexeme IDs without 'dannet' property")
+
+        if not hasattr(self, 'df'):
+            raise ValueError("DataFrame not available")
+
+        print("Finding DanNet IDs for lexemes")
+        self.setup_wbi()
+
+        for lexeme_id in tqdm(self.no_dannet_lexeme_ids, desc="Processing lexemes"):
+            logger.info(f"Working on {lexeme_id}")
+            lexeme = self.wbi.lexeme.get(entity_id=lexeme_id)
+            lemma = lexeme.lemmas.get(language="da")[0]
+            logger.info(f"Got lemma {lemma} for {lexeme.get_entity_url()}")
+
+            match = self.df[self.df['form'] == lemma]
+
+            if not match.empty:
+                dannet_id = match.iloc[0]['id']
+                logger.info(f"Found match! DanNet 2.2. ID: {dannet_id}")
+                exit()
+                self.upload_dannet_id_match(lexeme=lexeme, dannet_id=dannet_id)
+
+    def setup_wbi(self):
+        if not self.wbi:
+            self.wbi = WikibaseIntegrator(
+                login=Login(user=config.user_name, password=config.bot_password)
+            )
+
+    def upload_dannet_id_match(self, lexeme: LexemeEntity, dannet_id: str):
+        if not lexeme:
+            raise ValueError("no lexeme")
+        if not dannet_id:
+            raise ValueError("no dannet_id")
+        self.setup_wbi()
+        claim = ExternalID(prop_nr="P6140", value=dannet_id)
+        lexeme.claims.add(claims=[claim])
+        lexeme.write()
+
+# Usage
+url_zip_handler = ZipFileHandler(zip_file_path="https://repository.clarin.dk/repository/xmlui/bitstream/handle/20.500.12115/25/DanNet-2.2_owl.zip")
+url_zip_handler.download_zip()  # Downloads the zip file with progress
+url_zip_handler.unzip_content()
+url_zip_handler.print_head_unzipped_content()
+url_zip_handler.extract_forms()  # Extracts forms from the downloaded zip file
+
+for form in url_zip_handler.forms:
+    print(f"ID: {form.id}, Form: {form.form}")
+
+url_zip_handler.create_dataframe_and_export_csv()
+url_zip_handler.fetch_danish_lexeme_ids()
